4959fbf635a9b45a80a0fb1c500cbcb7ed0b1c0f
proHarshang <harshang.developer@gmail.com>
2023-09-08 18:00:23 +0530

    combained parts

--- index.php
+++ index.php
@@ -1,9 +1,54 @@
-<?php
-require_once 'parts/header.php';
-require_once 'parts/nav.php';
-?>
+<!DOCTYPE html>
+<html lang="en">
 
-<?php
-require_once 'parts/scripts.php';
-require_once 'parts/footer.php';
-?>
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="icon" type="image/x-icon" href="images/favicon.ico">
+    <title>Dhwity Enterprise</title>
+    <link rel="stylesheet" href="css/style.css" />
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
+</head>
+
+<body>
+    <!-- preloader -->
+    <div class="container-fluid" id="preloader">
+        <div class="d-flex flex-column align-items-center">
+            <img src="images/outer.png" class="outer_img">
+            <img src="images/inner.png" class="inner_img">
+        </div>
+    </div>
+
+    <!-- brand nav  -->
+    <nav id="brand">
+        <div id="header_logo">
+            <img src="images/outer.png" class="outer_img">
+            <img src="images/inner.png" class="inner_img">
+        </div>
+        <div class="heading">
+            <h1><span>Dhwity</span> <span>Enterprise</span></h1>
+            <span class="tagline">Pure and Flavourful</span>
+        </div>
+    </nav>
+
+    <p class="info_paragraph fs-6 lh-sm mx-auto my-5">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta nemo, eaque voluptate magni deserunt aut exercitationem quia quae modi voluptatibus. A, reiciendis ex? Asperiores quidem, laboriosam expedita dolor quas amet ipsum.</p>
+
+    <!-- fixed whatsapp chat btn -->
+    <a id="whatsapp_chat"><img src="images/whatsapp_chat.svg" alt="Whatsapp chat icon"></a>
+
+    <footer>
+
+    </footer>
+
+    <!-- bootstrap cdn  -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <!-- ajax & jquery cdn -->
+    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script> -->
+    <!-- script.js link-->
+    <script src="script.js"></script>
+
+</body>
+
+</html>
--- parts/footer.php
+++
@@ -1,5 +0,0 @@
-<a id="whatsapp_chat"><img src="images/whatsapp_chat.svg" alt="Whatsapp chat icon"></a>
-<footer>
-    
-</footer>
-</body>
