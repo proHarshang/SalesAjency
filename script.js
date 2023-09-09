6a5d67842508d67ffb51547108a406892f4a5155
proHarshang <harshang.developer@gmail.com>
2023-09-09 20:09:01 +0530

    All features added

--- script.js
+++ script.js
@@ -17,29 +17,79 @@ window.addEventListener("resize", function () {
   header_logo.style.height = lineAnim_header.offsetHeight - 50 + "px";
 });
 
-// product function to create a product
-
-function product(title) {
-  let elem = `
-  <div class="product_card">
-                    <h4 class="title fw-bold">${title}</h4>
+// grab perticular data of clicked element into the modal
+let product_wrapper_in_modal = document.querySelector(
+  ".product_wrapper_in_modal"
+);
+var all_product = document.getElementById("all_product");
+all_product.addEventListener("show.bs.modal", function (event) {
+  var button = event.relatedTarget;
+  var name = button.getAttribute("data-name");
+  var img = button.getAttribute("data-img");
+  all_product.querySelector(".modal-header .modal-title").textContent = name;
+  all_product.querySelector(".modal-header img").src = `images/${img}.webp`;
+  all_product.querySelector(".modal-header img").alt = name;
+  product_wrapper_in_modal.innerHTML = "";
+  if (name === "Vasant Masala") {
+    product_array.vasantMasala.forEach((e) => {
+      let msg = `I want to order ${e} from ${name}`;
+      product_wrapper_in_modal.innerHTML += `<div class="product_card">
+                    <h4 class="title fw-bold">${e}</h4>
                     <div class="company d-flex flex-column">
-                        <span class="text-secondary">Company</span>
-                        <span class="company_name">Undefined</span>
+                        <span class="text-secondary lh-1">Company</span>
+                        <span class="company_name">${name}</span>
                     </div>
-                    <a href="" class="whatsapp_order"><img src="images/whatsapp_order.svg" alt="Whatsapp Order"><span>Order Now</span></a>
-                </div>
-  `;
-  return elem;
-}
-
-// product("title");
-
-// set company name dynamically
-document.querySelectorAll(".product_card").forEach((e) => {
-  let wrapper = e.parentElement;
-  let companyname = wrapper.getAttribute("data-company");
-  e.querySelectorAll(".company_name").forEach((elem) => {
-    elem.textContent = companyname;
-  });
+                    <a href="https://wa.me/919898490529?text=${msg}" target="_blank" class="whatsapp_order"><img src="images/whatsapp_order.svg" alt="Whatsapp Order"><span>Order Now</span></a>
+                </div>`;
+    });
+  } else if (name === "Perfetti") {
+    product_array.perfetti.forEach((e) => {
+      let msg = `I want to order ${e} from ${name}`;
+      product_wrapper_in_modal.innerHTML += `<div class="product_card">
+                    <h4 class="title fw-bold">${e}</h4>
+                    <div class="company d-flex flex-column">
+                        <span class="text-secondary lh-1">Company</span>
+                        <span class="company_name">${name}</span>
+                    </div>
+                    <a href="https://wa.me/919898490529?text=${msg}" target="_blank" class="whatsapp_order"><img src="images/whatsapp_order.svg" alt="Whatsapp Order"><span>Order Now</span></a>
+                </div>`;
+    });
+  } else if (name === "Thames") {
+    product_array.thames.forEach((e) => {
+      let msg = `I want to order ${e} from ${name}`;
+      // product_wrapper_in_modal.appendChild = product(e, name);
+      product_wrapper_in_modal.innerHTML += `<div class="product_card">
+                    <h4 class="title fw-bold">${e}</h4>
+                    <div class="company d-flex flex-column">
+                        <span class="text-secondary lh-1">Company</span>
+                        <span class="company_name">${name}</span>
+                    </div>
+                    <a href="https://wa.me/919898490529?text=${msg}" target="_blank" class="whatsapp_order"><img src="images/whatsapp_order.svg" alt="Whatsapp Order"><span>Order Now</span></a>
+                </div>`;
+    });
+  } else if (name === "Eveready") {
+    product_array.eveready.forEach((e) => {
+      let msg = `I want to order ${e} from ${name}`;
+      product_wrapper_in_modal.innerHTML += `<div class="product_card">
+                    <h4 class="title fw-bold">${e}</h4>
+                    <div class="company d-flex flex-column">
+                        <span class="text-secondary lh-1">Company</span>
+                        <span class="company_name">${name}</span>
+                    </div>
+                    <a href="https://wa.me/919898490529?text=${msg}" target="_blank" class="whatsapp_order"><img src="images/whatsapp_order.svg" alt="Whatsapp Order"><span>Order Now</span></a>
+                </div>`;
+    });
+  } else if (name === "Jivraj9") {
+    product_array.jivraj9.forEach((e) => {
+      let msg = `I want to order ${e} from ${name}`;
+      product_wrapper_in_modal.innerHTML += `<div class="product_card">
+                    <h4 class="title fw-bold">${e}</h4>
+                    <div class="company d-flex flex-column">
+                        <span class="text-secondary lh-1">Company</span>
+                        <span class="company_name">${name}</span>
+                    </div>
+                    <a href="https://wa.me/919898490529?text=${msg}" target="_blank" class="whatsapp_order"><img src="images/whatsapp_order.svg" alt="Whatsapp Order"><span>Order Now</span></a>
+                </div>`;
+    });
+  }
 });
